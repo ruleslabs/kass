74b6b422cbbf2e789c651be48bc011c3daf78abf
xshitaka <clanier.dev@gmail.com>
2023-03-04 20:36:59 +0100

    L1 instance creation must be requested from L2 Kass

--- src/ethereum/KassBridge.sol
+++ src/ethereum/KassBridge.sol
@@ -2,14 +2,16 @@
 
 pragma solidity ^0.8.19;
 
-import "openzeppelin-contracts/contracts/utils/Context.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 import "./interfaces/IStarknetMessaging.sol";
 import "./KassUtils.sol";
 import "./ERC1155/KassERC1155.sol";
 
-contract KassBridge is Context {
+contract KassBridge is Ownable {
     IStarknetMessaging private starknetMessaging;
+
+    uint256 private l2KassAddress;
 
     // CONSTRUCTOR
 
@@ -19,24 +21,34 @@ contract KassBridge is Context {
 
     // GETTERS
 
-    function l1Address(uint256 l2TokenAddress) public view returns (address) {
+    function l1TokenAddress(uint256 l2TokenAddress) public view returns (address) {
         return KassUtils.computeAddress(address(this), type(KassERC1155).creationCode, bytes32(l2TokenAddress));
+    }
+
+    // SETTERS
+
+    function setL2KassAddress(uint256 _l2KassAddress) public onlyOwner {
+        l2KassAddress = _l2KassAddress;
     }
 
     // BUSINESS LOGIC
 
-    function createL1Instance(uint256 tokenL2Address, string[] calldata uri) public returns (address) {
+    function createL1Instance(uint256 l2TokenAddress, string[] calldata uri) public returns (address) {
         // compute L1 instance request payload
-        uint256[] memory payload = new uint256[](uri.length);
+        uint256[] memory payload = new uint256[](uri.length + 1);
+
+        // load L2 token address
+        payload[0] = l2TokenAddress;
+
         for (uint8 i = 0; i < uri.length; ++i) {
-            payload[i] = KassUtils.strToUint256(uri[i]);
+            payload[i + 1] = KassUtils.strToUint256(uri[i]);
         }
 
         // consume L1 instance request message
-        starknetMessaging.consumeMessageFromL2(tokenL2Address, payload);
+        starknetMessaging.consumeMessageFromL2(l2KassAddress, payload);
 
         // deploy Kass ERC1155 and set URI
-        KassERC1155 l1TokenInstance = new KassERC1155{salt: bytes32(tokenL2Address)}();
+        KassERC1155 l1TokenInstance = new KassERC1155{salt: bytes32(l2TokenAddress)}();
         l1TokenInstance.setURI(KassUtils.concat(uri));
 
         return address(l1TokenInstance);
--- test/KassBridge.t.sol
+++ test/KassBridge.t.sol
@@ -10,6 +10,7 @@ import "../src/ethereum/ERC1155/KassERC1155.sol";
 import "../src/ethereum/mocks/StarknetMessagingMock.sol";
 
 address constant STARKNET_MESSAGNING_ADDRESS = address(uint160(uint256(keccak256("starknet messaging"))));
+uint256 constant L2_KASS_ADDRESS = uint256(keccak256("L2 Kass"));
 uint256 constant L2_TOKEN_ADDRESS = uint256(keccak256("L2 token"));
 
 contract KassBridgeTest is Test {
@@ -17,51 +18,69 @@ contract KassBridgeTest is Test {
     address private starknetMessagingAddress;
 
     // solhint-disable-next-line var-name-mixedcase
-    string[] private TOKEN_URI;
+    string[] private L2_TOKEN_URI;
     // solhint-disable-next-line var-name-mixedcase
-    uint256[] private TOKEN_URI_PAYLOAD;
+    uint256[] private L1_TOKEN_REQUEST_PAYLOAD;
 
     // SETUP
 
     function setUp() public {
+        // setup starknet messaging mock
         starknetMessagingAddress = address(new StarknetMessagingMock());
+
+        // setup bridge
         kassBridge = new KassBridge(starknetMessagingAddress);
+        kassBridge.setL2KassAddress(L2_KASS_ADDRESS);
 
-        // token uri
-        TOKEN_URI = new string[](3);
-        TOKEN_URI[0] = "https://api.rule";
-        TOKEN_URI[1] = "s.art/metadata/{";
-        TOKEN_URI[2] = "id}.json";
+        // L2 token uri
+        L2_TOKEN_URI = new string[](3);
+        L2_TOKEN_URI[0] = "https://api.rule";
+        L2_TOKEN_URI[1] = "s.art/metadata/{";
+        L2_TOKEN_URI[2] = "id}.json";
 
-        // token uri payload
-        TOKEN_URI_PAYLOAD = new uint256[](TOKEN_URI.length);
-        for (uint8 i = 0; i < TOKEN_URI.length; ++i) {
-            TOKEN_URI_PAYLOAD[i] = KassUtils.strToUint256(TOKEN_URI[i]);
+        // L1 token request payload
+        L1_TOKEN_REQUEST_PAYLOAD = new uint256[](L2_TOKEN_URI.length + 1);
+
+        // load L2 token address
+        L1_TOKEN_REQUEST_PAYLOAD[0] = L2_TOKEN_ADDRESS;
+
+        // load L2 token URI
+        for (uint8 i = 0; i < L2_TOKEN_URI.length; ++i) {
+            L1_TOKEN_REQUEST_PAYLOAD[i + 1] = KassUtils.strToUint256(L2_TOKEN_URI[i]);
         }
     }
 
     // HELPERS
 
-    function createL1Instance() private returns (KassERC1155) {
+    function createL1Instance() private returns (KassERC1155 kassERC1155) {
         // prepare L1 instance creation message from L2
         vm.mockCall(
             starknetMessagingAddress,
             abi.encodeWithSelector(
                 IStarknetMessaging.consumeMessageFromL2.selector,
-                L2_TOKEN_ADDRESS,
-                TOKEN_URI_PAYLOAD
+                L2_KASS_ADDRESS,
+                L1_TOKEN_REQUEST_PAYLOAD
             ),
             abi.encode()
         );
 
         // create and return L1 instance
-        return KassERC1155(kassBridge.createL1Instance(L2_TOKEN_ADDRESS, TOKEN_URI));
+        kassERC1155 = KassERC1155(kassBridge.createL1Instance(L2_TOKEN_ADDRESS, L2_TOKEN_URI));
+
+        // clear mocked calls
+        vm.clearMockedCalls();
     }
 
     // TESTS
 
-    function testL1InstanceCreation() public {
-        // GIVEN
+    function testCannotUpdateL2KassAddressIfNotOwner() public {
+        // THEN
+        vm.prank(address(0x42));
+        vm.expectRevert("Ownable: caller is not the owner");
+        kassBridge.setL2KassAddress(0xdead);
+    }
+
+    function testL1TokenInstanceComputedAddress() public {
         // pre compute address
         address computedL1TokenAddress = KassUtils.computeAddress(
             address(kassBridge),
@@ -69,12 +88,27 @@ contract KassBridgeTest is Test {
             bytes32(L2_TOKEN_ADDRESS)
         );
 
-        // WHEN
         // create L1 instance
         KassERC1155 l1TokenInstance = createL1Instance();
 
         // THEN
         assertEq(computedL1TokenAddress, address(l1TokenInstance));
-        assertEq(l1TokenInstance.uri(0), KassUtils.concat(TOKEN_URI));
+    }
+
+    function testL1TokenInstanceUri() public {
+        // create L1 instance
+        KassERC1155 l1TokenInstance = createL1Instance();
+
+        // THEN
+        assertEq(l1TokenInstance.uri(0), KassUtils.concat(L2_TOKEN_URI));
+    }
+
+    function testCannotUpdateL1TokenInstanceUri() public {
+        // create L1 instance
+        KassERC1155 l1TokenInstance = createL1Instance();
+
+        // THEN
+        vm.expectRevert("KassERC1155: URI already set");
+        l1TokenInstance.setURI("foo");
     }
 }
